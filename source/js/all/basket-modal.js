d141cf7a7f4eb72734dfd73ccbff412dead1f349
Denys Hutsal <denyshutsal@gmail.com>
2019-09-28 10:31:22 +0300

    added the ability to close a basket modal by pressing the Esc key

--- source/js/all/basket-modal.js
+++ source/js/all/basket-modal.js
@@ -7,15 +7,26 @@ const cardLink = document.querySelector('.card__link');
 const basketModal = document.querySelector('.basket-modal');
 const overlayModal = document.querySelector('.overlay-modal');
 
-// basket modal
+// opening a modal basket window
 cardLink.addEventListener('click', function (event) {
   event.preventDefault();
   basketModal.classList.add('basket-modal--opened');
   overlayModal.classList.add('overlay-modal--opened');
 });
 
-// overlay modal
+// closing the modal basket window with the Esc key
+window.addEventListener('keydown', function(event) {
+  if (event.code === 'Escape') {
+    basketModalHandler()
+  }
+});
+
+// сlosing the modal basket window by clicking on the overlay
 overlayModal.addEventListener('click', function () {
+  basketModalHandler()
+});
+
+function basketModalHandler() {
   basketModal.classList.remove('basket-modal--opened');
   overlayModal.classList.remove('overlay-modal--opened');
-});
+}
